c76e633745c856fed53a3632801511811503ca95
V-2Schneider <jodieczeci@gmail.com>
2018-11-24 21:42:01 +0100

    wykrywa kij! lecz nadal wszystko naokoło

--- Test_approaches/Cue_Stick.py
+++ Test_approaches/Cue_Stick.py
@@ -24,11 +24,13 @@ def Display(filename1, filename2):
     img2 = cv2.imread('../Pictures/Small/' + filename2, cv2.IMREAD_COLOR)
     img2 = cv2.GaussianBlur(img2, (5, 5), 0)
 
-    cv2.createTrackbar('H', 'image', 10, 245, nothing)
-    cv2.createTrackbar('S', 'image', 100, 255, nothing)
-    cv2.createTrackbar('V', 'image', 100, 255, nothing)
+    cv2.createTrackbar('H', 'image', 14, 245, nothing)
+    cv2.createTrackbar('S', 'image', 8, 255, nothing)
+    cv2.createTrackbar('V', 'image', 88, 255, nothing)
     cv2.createTrackbar('S2', 'image', 255, 255, nothing)
     cv2.createTrackbar('V2', 'image', 255, 255, nothing)
+    cv2.createTrackbar('linel', 'image', 180, 360, nothing)
+    cv2.createTrackbar('lineg', 'image', 100, 200, nothing)
 
     while (1):
 
@@ -41,8 +43,10 @@ def Display(filename1, filename2):
         v = cv2.getTrackbarPos('V', 'image')
         s2 = cv2.getTrackbarPos('S2', 'image')
         v2 = cv2.getTrackbarPos('V2', 'image')
-        lower_stick = np.array([h - 10, s, v])
-        upper_stick = np.array([h + 10, s2, v2])
+        linel = cv2.getTrackbarPos('linel', 'image')
+        lineg = cv2.getTrackbarPos('lineg', 'image')
+        lower_stick = np.array([h - 5, s, v])
+        upper_stick = np.array([h + 5, s2, v2])
 
         mask1 = cv2.inRange(hsv1, lower_stick, upper_stick)
         mask2 = cv2.inRange(hsv2, lower_stick, upper_stick)
@@ -53,11 +57,12 @@ def Display(filename1, filename2):
         img3 = np.hstack((img1, img2))
 
         res = cv2.bitwise_and(img3, img3, mask=numpy_horizontal)
+        #edges2 = cv2.Canny(img3, 200, 150, apertureSize=3)
         edges = cv2.Canny(res, 200, 150, apertureSize=3)
 
         minLineLength = 10
-        maxLineGap = 1
-        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+        maxLineGap = 10
+        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 10, minLineLength, maxLineGap)
         if (not (lines is None or len(lines) == 0)):
             for x in range(0, len(lines)):
                 for x1, y1, x2, y2 in lines[x]:
@@ -68,4 +73,4 @@ def Display(filename1, filename2):
     cv2.destroyAllWindows()
 
 
-Display(Pictures()[4], Pictures()[5])
+Display(Pictures()[0], Pictures()[1])

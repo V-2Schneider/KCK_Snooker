27fa551e32e30ed01b3f346ae83543a54beb4c02
V-2Schneider <jodieczeci@gmail.com>
2018-11-27 18:28:17 +0100

    Added an actual video (please delete later!), support for it and cleaned up the code

--- Test_approaches/Cue_Stick.py
+++ Test_approaches/Cue_Stick.py
@@ -2,11 +2,16 @@ import cv2
 import numpy as np
 from Pic import Pictures
 
+cap = cv2.VideoCapture('../Video/Snooker.mp4')
+if (cap.isOpened()== False):
+  print("Error opening video stream or file")
+
 def nothing(x):
     pass
 
+# warning! Cue_Stick is probably not needed anymore!
 def Cue_Stick(filename):
-    cv2.namedWindow('image')
+
     # Open the file
     img = cv2.imread('../Pictures/Small/' + filename, cv2.IMREAD_COLOR)
     img = cv2.GaussianBlur(img, (5,5), 0)
@@ -14,15 +19,11 @@ def Cue_Stick(filename):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     return hsv
 
-def Display(filename1, filename2):
-    hsv1 = Cue_Stick(filename1)
-    hsv2 = Cue_Stick(filename2)
+def Display():
+    cv2.namedWindow('image')
 
-    img1 = cv2.imread('../Pictures/Small/' + filename1, cv2.IMREAD_COLOR)
-    img1 = cv2.GaussianBlur(img1, (5, 5), 0)
+    # Convert BGR to HSV
 
-    img2 = cv2.imread('../Pictures/Small/' + filename2, cv2.IMREAD_COLOR)
-    img2 = cv2.GaussianBlur(img2, (5, 5), 0)
 
     cv2.createTrackbar('H', 'image', 14, 245, nothing)
     cv2.createTrackbar('S', 'image', 8, 255, nothing)
@@ -38,39 +39,35 @@ def Display(filename1, filename2):
         if k == 27:
             break
 
+        _, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
         h = cv2.getTrackbarPos('H', 'image')
         s = cv2.getTrackbarPos('S', 'image')
         v = cv2.getTrackbarPos('V', 'image')
         s2 = cv2.getTrackbarPos('S2', 'image')
         v2 = cv2.getTrackbarPos('V2', 'image')
-        linel = cv2.getTrackbarPos('linel', 'image')
-        lineg = cv2.getTrackbarPos('lineg', 'image')
         lower_stick = np.array([h - 5, s, v])
         upper_stick = np.array([h + 5, s2, v2])
 
-        mask1 = cv2.inRange(hsv1, lower_stick, upper_stick)
-        mask2 = cv2.inRange(hsv2, lower_stick, upper_stick)
+        mask = cv2.inRange(hsv, lower_stick, upper_stick)
 
-        numpy_horizontal = np.hstack((mask1, mask2))
+        res = cv2.bitwise_and(frame, frame, mask=mask)
 
-
-        img3 = np.hstack((img1, img2))
-
-        res = cv2.bitwise_and(img3, img3, mask=numpy_horizontal)
-        #edges2 = cv2.Canny(img3, 200, 150, apertureSize=3)
         edges = cv2.Canny(res, 200, 150, apertureSize=3)
 
-        minLineLength = 10
-        maxLineGap = 10
-        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 10, minLineLength, maxLineGap)
+        minLineLengthVal = 20
+        maxLineGapVal = 3
+        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 10, minLineLength=minLineLengthVal, maxLineGap=maxLineGapVal)
         if (not (lines is None or len(lines) == 0)):
             for x in range(0, len(lines)):
                 for x1, y1, x2, y2 in lines[x]:
                     cv2.line(res, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
         cv2.imshow('image', res)
+        cv2.imshow('frame', frame)
         #cv2.imshow('image', edges)
     cv2.destroyAllWindows()
 
 
-Display(Pictures()[0], Pictures()[1])
+Display()
